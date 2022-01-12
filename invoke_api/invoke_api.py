aa6f0d46c111b5737fc2b45ca21dbe2952efa4f1
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-01-12 18:06:14 -0500

    initial commit, including CDK bootstrap

---
+++ invoke_api/invoke_api.py
@@ -0,0 +1,8 @@
+import os
+import json
+# from urllib.parse import urlencode
+# import urllib3, ssl
+
+def lambda_handler(event, context):
+
+    return { "status" : "success" }
--- uploader/uploader_stack.py
+++ uploader/uploader_stack.py
@@ -1,7 +1,12 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    CfnParameter,
+    aws_iam as iam,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_lambda as _lambda,
+    aws_events as events
 )
 from constructs import Construct
 
@@ -10,10 +15,44 @@ class UploaderStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # udl_credential = CfnParameter(self,
+        #         "UDLcredential", type="String",
+        #         description="Access credentials encoded using base64")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UploaderQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        runtime = _lambda.Runtime.PYTHON_3_8
+
+        # need to make boundary policy an optional parameter. update role definitions accordingly.
+        permissions_boundary=iam.ManagedPolicy.from_managed_policy_name(self, 'PermissionBoundaryLambda', "T_PROJADMIN_U")
+        iam.PermissionsBoundary.of(self).apply(permissions_boundary)
+
+
+        retry_queue = sqs.Queue(
+            self, "RetryQueue",
+            visibility_timeout=Duration.seconds(60),
+        )
+
+        # SNS for status
+
+        # role(s) for lambdas?
+
+        # normal lambda permissions, eventbridge actions, s3 read,write,list, SNS, SQS
+
+        # lambdas
+        # invoke_api
+
+        invoke_api_lambda = _lambda.Function(
+                self, 'InvokeApi',
+                runtime=runtime,
+                code=_lambda.Code.from_asset('invoke_api'),
+                handler='invoke_api.lambda_handler',
+                environment= {
+                    # 'CREDENTIAL' : udl_credential.value_as_string
+                },
+                # layers = [ pandas_layer ]
+                # role=lambda_role
+            )
+
+
+
+
+        # event subscriptions
