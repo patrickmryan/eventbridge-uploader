387e448d8a245f6b7a82b4bfddba46ece46e6817
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-01-14 15:34:33 -0500

    starting to add resources

--- invoke_api/invoke_api.py
+++ invoke_api/invoke_api.py
@@ -1,8 +1,13 @@
 import os
 import json
+import boto3
 # from urllib.parse import urlencode
 # import urllib3, ssl
 
+
+
 def lambda_handler(event, context):
+
+    # for testing, copy the object to another s3 bucket
 
     return { "status" : "success" }
--- uploader/uploader_stack.py
+++ uploader/uploader_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     CfnParameter,
     aws_iam as iam,
     aws_sqs as sqs,
+    aws_s3 as s3,
     aws_sns as sns,
     aws_lambda as _lambda,
     aws_events as events
@@ -15,15 +16,26 @@ class UploaderStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # udl_credential = CfnParameter(self,
-        #         "UDLcredential", type="String",
-        #         description="Access credentials encoded using base64")
+        outgoing_bucket = CfnParameter(self,
+                "OutgoingBucket", type="String",
+                description="Bucket for data to be submitted to the API")
 
-        runtime = _lambda.Runtime.PYTHON_3_8
+        permissions_boundary_policy = CfnParameter(self,
+                "PermissionsBoundaryPolicy", type="String",
+                description="(optional) Policy to be added as permissions boundary to all IAM roles")
+
 
         # need to make boundary policy an optional parameter. update role definitions accordingly.
         permissions_boundary=iam.ManagedPolicy.from_managed_policy_name(self, 'PermissionBoundaryLambda', "T_PROJADMIN_U")
         iam.PermissionsBoundary.of(self).apply(permissions_boundary)
+
+
+        runtime = _lambda.Runtime.PYTHON_3_8
+
+        # topic
+
+        # buckets
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_s3/Bucket.html
 
 
         retry_queue = sqs.Queue(
@@ -32,13 +44,29 @@ class UploaderStack(Stack):
         )
 
         # SNS for status
+        new_object_topic = sns.Topic(self,
+                        "NewObjectTopic")
+        topic_policy = sns.TopicPolicy(self,
+                    "NewObjectTopicPolicy",
+                    topics=[new_object_topic])
+        topic_policy.document.add_statements(
+                iam.PolicyStatement(
+                actions=["sns:Subscribe"],
+                principals=[
+                    iam.ServicePrincipal("s3.amazonaws.com")
+                    # iam.AnyPrincipal()
+                ],
+                resources=[new_object_topic.topic_arn]))
+
 
         # role(s) for lambdas?
-        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_lambda/Function.html
+
 
         # normal lambda permissions, eventbridge actions, s3 read,write,list, SNS, SQS
 
         # lambdas
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_lambda/Function.html
+
         # invoke_api
 
         invoke_api_lambda = _lambda.Function(
